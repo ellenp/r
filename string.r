9aeb21105dbddb1d1a482b6cf20e7c18de10ec77
ellenp <pates.ellen@gmail.com>
2025-06-15 14:02:47 +0800

    string.r

---
+++ string.r
@@ -0,0 +1,21 @@
+
+str <- "Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."
+
+str
+
+cat(str)
+
+grepl("H", str)
+grepl("Hello", str)
+grepl("X", str)
+
+nchar(str)
+
+str <- "I want to inser a backslash \\ here and a new line \n here and a carriage return \r here and a tab \t here and a backspace \b here."
+
+str
+
+cat(str)
